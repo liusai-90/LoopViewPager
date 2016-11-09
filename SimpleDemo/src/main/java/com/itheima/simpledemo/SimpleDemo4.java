67475ae55492ec56433e89079dbfd890310756a3
yaoshouyun <yaoshouyun@itcast.cn>
2016-11-09 17:30:24 +0800

    v1.1.0

--- SimpleDemo/src/main/java/com/itheima/simpledemo/SimpleDemo4.java
+++ SimpleDemo/src/main/java/com/itheima/simpledemo/SimpleDemo4.java
@@ -10,13 +10,19 @@ import java.util.List;
 
 public class SimpleDemo4 extends AppCompatActivity {
 
-    private int[] imageData;
+    private String[] imageData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_demo4);
-        imageData = new int[]{R.mipmap.image1, R.mipmap.image2, R.mipmap.image3, R.mipmap.image4, R.mipmap.image5, R.mipmap.image6};
+        imageData = new String[]{
+                "http://d.hiphotos.baidu.com/image/h%3D200/sign=72b32dc4b719ebc4df787199b227cf79/58ee3d6d55fbb2fb48944ab34b4a20a44723dcd7.jpg",
+                "http://pic.4j4j.cn/upload/pic/20130815/31e652fe2d.jpg",
+                "http://pic.4j4j.cn/upload/pic/20130815/5e604404fe.jpg",
+                "http://pic.4j4j.cn/upload/pic/20130909/681ebf9d64.jpg",
+                "http://d.hiphotos.baidu.com/image/pic/item/54fbb2fb43166d22dc28839a442309f79052d265.jpg",
+        };
         ((LoopViewPager) findViewById(R.id.lvp_pager)).setImgData(imageData);
     }
 
