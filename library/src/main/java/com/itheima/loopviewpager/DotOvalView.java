3115f190a25f1fe54278ad31e925bafd6d4eb05b
yaoshouyun <yaoshouyun@itcast.cn>
2016-11-10 10:28:34 +0800

    v1.1.0

--- library/src/main/java/com/itheima/loopviewpager/DotOvalView.java
+++ library/src/main/java/com/itheima/loopviewpager/DotOvalView.java
@@ -8,61 +8,40 @@ import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 public class DotOvalView extends ImageView {
 
+    private int color;
+    private Paint paint;
+
     public DotOvalView(Context context) {
         super(context);
+        paint = new Paint();
     }
-
-    Bitmap bitmap;
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Drawable drawable = getDrawable();
-        if (drawable != null) {
-            bitmap = ((BitmapDrawable) drawable).getBitmap();
-            Bitmap bitmap2 = getRoundBitmap();
-//            final Rect rectSrc = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
-//            final Rect rectDest = new Rect(0, 0, getWidth(), getHeight());
-
-            paint.reset();
-            canvas.drawBitmap(bitmap2, 0, 0, paint);
-        }
+        Bitmap bitmap = getRoundBitmap();
+        paint.reset();
+        canvas.drawBitmap(bitmap, 0, 0, paint);
     }
 
-    Paint paint;
-
     private Bitmap getRoundBitmap() {
-        Toast.makeText(getContext(), "getRoundBitmap", Toast.LENGTH_SHORT).show();
-
         Bitmap output = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(output);
-
-        paint = new Paint();
-
-//        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
-//        final RectF rectF = new RectF(rect);
         paint.setAntiAlias(true);
-        paint.setFilterBitmap(false);
-        paint.setColor(Color.YELLOW);
-//        int x = bitmap.getWidth();
-//        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
-//        paint.setXfermode(Xfermode.)
-//        canvas.drawOval(new RectF(5, 5, getWidth(), getHeight()), paint);
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        paint.setColor(color);
         canvas.drawOval(new RectF(0, 0, getWidth(), getHeight()), paint);
-//        paint.setColor(Color.GREEN);
-
-//        canvas.drawARGB(0, 0, 0, 0);
-        paint.setXfermode(null);
-        canvas.drawBitmap(bitmap,0, 0, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawColor(Color.TRANSPARENT);
         return output;
+    }
+
+    public void change(int color) {
+        this.color = color;
+        invalidate();
     }
 
 }
